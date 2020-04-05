bbed2707fb10a0986901817baa090e984fbc1c65
Mrhuma <22532034+Mrhuma@users.noreply.github.com>
2020-04-05 13:03:21 -0600

    Removed the isPlaying bool from the Song object and fixed the album art display

--- GooglePlayMusicPlayback/Song.cs
+++ GooglePlayMusicPlayback/Song.cs
@@ -2,20 +2,16 @@
 {
     class Song
     {
-        private bool _Playing;
         private string _Title;
         private string _Artist;
         private string _albumArt;
-
-        public bool Playing { get { return _Playing; } set { _Playing = value; } }
         public string Title { get { return _Title; } set { _Title = value; } }
         public string Artist { get { return _Artist; } set { _Artist = value; } }
 
         public string albumArt { get { return _albumArt; } set { _albumArt = value; } }
 
-        public Song(bool playing, string title, string artist, string albumart)
+        public Song(string title, string artist, string albumart)
         {
-            _Playing = playing;
             _Title = title;
             _Artist = artist;
             _albumArt = albumart;
